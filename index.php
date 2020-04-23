6c4dc7156f260d8cdc659e4fd1c039a574bae8cf
jcfauquenot <jcfauquenot@gmail.com>
2020-04-23 09:24:17 +0200

    form react with php server

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header('Access-Control-Allow-Headers: Content-Type');
-// $rest_json = file_get_contents("php://input");
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
 // https://github.com/malithmcr/react-php-contact-form/blob/master/api/contact/index.php
 
+$rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-if( empty($_POST['firstName']) && empty($_POST['email']) ) {
-    echo json_encode(
-        [
-           "sent" => false,
-           "message" => "ca ne marche pas du tout"
+$didi = $_POST['fname']."ceci est geniale";
+
+http_response_code(200);
+
+echo json_encode(
+        [ 
+           "post" => $didi,
+           "robert" => $_POST['lname']
+
         ]
     ); 
-    exit();
-}
-
-if ($_POST){
-    //@important: Please change this before using
-    http_response_code(200);
-    $subject = 'Contact from: ' . $_POST['firstName'];
-    $from = $_POST['email'];
-    $message = $_POST['msg'];       
-    //Actual sending email
-
-} else {
- // tell the user about error
- echo json_encode(
-     [
-        "sent" => false,
-        "message" => $SendMailFailederrorMessage
-     ]
- );
-}
